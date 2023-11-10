2d75bb207b7f62e17cac10fdf413b6511fddfaeb
lurian <lurian-code@protonmail.com>
2023-11-10 17:06:17 +0100

    refactor(home-manager): Update dotfiles fetch and installation

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -2,9 +2,20 @@
 {
   pkgs,
   ...
-}: {
-  ".config/nvim".source = pkgs.fetchgit {
-     url = "https://github.com/lur1an/dotfiles";
-     sparseCheckout = [ "nvim" ];
+}: 
+let
+  dotfiles = pkgs.stdenv.mkDerivation {
+    name = "dotfiles";
+    src = pkgs.fetchgit {
+      url = "https://github.com/lur1an/dotfiles";
+    };
+    installPhase = ''
+      mkdir -p $out
+      cp -r . $out/
+    '';
   };
+in
+{
+  home.file.".config/nvim".source = "${dotfiles}/nvim";
 }
+
