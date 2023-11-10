09ffd5f9c89abe43f0417c5fc7a6df780d233581
lurian <lurian-code@protonmail.com>
2023-11-10 13:05:00 +0100

    feat(pipewire): Enable and configure ALSA in pipewire service

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -116,7 +116,7 @@
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire.enable = {
+  services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32bit = true;
