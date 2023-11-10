713d0c8b92c5fa8fbe5ec538f9d33754c0db439c
lurian <lurian-code@protonmail.com>
2023-11-10 17:09:40 +0100

    refactor(home-manager): Simplify dotfiles fetchgit usage

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -4,16 +4,19 @@
   ...
 }: 
 let
-  dotfiles = pkgs.stdenv.mkDerivation {
-    name = "dotfiles";
-    src = pkgs.fetchgit {
-      url = "https://github.com/lur1an/dotfiles";
-    };
-    installPhase = ''
-      mkdir -p $out
-      cp -r . $out/
-    '';
-  };
+  # dotfiles = pkgs.stdenv.mkDerivation {
+  #   name = "dotfiles";
+  #   src = pkgs.fetchgit {
+  #     url = "https://github.com/lur1an/dotfiles";
+  #   };
+  #   installPhase = ''
+  #     mkdir -p $out
+  #     cp -r . $out/
+  #   '';
+  # };
+  dotfiles = pkgs.fetchgit {
+    url = "https://github.com/lur1an/dotfiles"
+  }
 in
 {
   home.file.".config/nvim".source = "${dotfiles}/nvim";
