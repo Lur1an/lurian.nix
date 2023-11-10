0c4090e2ed20daa10c1d7e7e008a6906a4fd219b
lurian <lurian-code@protonmail.com>
2023-11-10 18:29:08 +0100

    refactor(home-manager): Update dotfiles source paths

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -6,7 +6,8 @@
   home.file.".config/nvim".source = ../dotfiles/nvim;
   home.file.".fonts".source = ../dotfiles/fonts;
   home.file.".config/alacritty".source = ../dotfiles/alacritty;
-  home.file.".zshrc".source = ../dotfiles/zshrc;
+  home.file.".zshrc".source = ../dotfiles/zsh/.zshrc;
+  home.file.".xresources".source = ../dotfiles/zsh/.xresources;
   home.file.".tmux.conf".source = ../dotfiles/tmux/.tmux.conf;
 }
 
