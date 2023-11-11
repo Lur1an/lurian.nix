5a1445956682b13c8d5d41f907bb50f3ac08273e
lurian <lurian-code@protonmail.com>
2023-11-11 13:42:42 +0100

    feat(nixos configuration): Enable KDE and exclude certain packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -96,6 +96,21 @@
   programs.zsh.enable = true;
   programs.dconf.enable = true;
 
+  # KDE
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    gwenview
+    okular
+    oxygen
+    khelpcenter
+    konsole
+    plasma-browser-integration
+    print-manager
+  ];
+
   # Hyprland stuff ------------------
 
   programs.hyprland = {
