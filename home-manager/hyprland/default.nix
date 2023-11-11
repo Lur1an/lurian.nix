5b346e5dd444cfd75368d629a579468927690ea7
lurian <lurian-code@protonmail.com>
2023-11-11 14:43:11 +0100

    refactor(hyprland): Remove extraConfig for wayland.windowManager

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -10,14 +10,5 @@
   imports = [
     inputs.hyprland.homeManagerModules.default
   ];
-
-  wayland.windowManager.hyprland.extraConfig = ''
-      $mod = SUPER
-
-      bind = $mod, B, exec, firefox
-
-      bind = $mod, S, exec, rofi -show drun -show-icons
-      bind = CTRL, T, exec, alacritty
-  '';
   wayland.windowManager.hyprland.enable = true;
 }
