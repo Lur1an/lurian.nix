12e42135a31b854a3318fa2f2d9b515c5213c778
lurian <lurian-code@protonmail.com>
2023-11-11 08:30:09 +0100

    refactor(nixos/hyprland): Remove redundant Wayland enablement

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -22,8 +22,6 @@
     videoDrivers = [ "nvidia" ];
   };
 
-  wayland.windowManager.hyprland.enable = true;
-
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
