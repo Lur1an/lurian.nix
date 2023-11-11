db67417b1d47c65e769ef8b2ee4410c0f24483b9
lurian <lurian-code@protonmail.com>
2023-11-11 14:48:44 +0100

    feat(hyprland): Add extra config for windowManager

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -11,4 +11,11 @@
     inputs.hyprland.homeManagerModules.default
   ];
   wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.extraConfig = ''
+    $mod = SUPER
+
+    bind = $mod, B, exec, firefox
+    bind = CTRL, T, exec, alacritty
+    bind = $mod, S, exec, rofi -show drun -show-icons
+  '';
 }
