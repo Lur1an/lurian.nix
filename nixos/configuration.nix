8821998049acc921c996ccc1546f38bf3fa2d508
lurian <lurian-code@protonmail.com>
2023-11-11 13:35:17 +0100

    chore(nixos): Remove gnome config, add rofi-wayland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,7 +19,6 @@
 
     # You can also split up your configuration and import pieces of it here:
     inputs.home-manager.nixosModules.home-manager
-    ./gnome.nix
   ];
 
 
@@ -164,6 +163,8 @@
     libnotify
     # wallpaper
     swww
+    # app-launcher
+    rofi-wayland
   ];
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
