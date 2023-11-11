b483ab7ee894ee82b66b2757a97091b0b523abf2
lurian <lurian-code@protonmail.com>
2023-11-11 14:32:24 +0100

    refactor(hyprland config): Simplify key binding configuration

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -12,29 +12,10 @@
   ];
 
   wayland.windowManager.hyprland.extraConfig = ''
-      $mod = SUPER
-
       bind = $mod, B, exec, firefox
 
-      bind = $mainMod, S, exec, rofi -show drun -show-icons
+      bind = $mod, S, exec, rofi -show drun -show-icons
       bind = CTRL, T, exec, alacritty
-
-      # workspaces
-      # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-      ${builtins.concatStringsSep "\n" (builtins.genList (
-          x: let
-            ws = let
-              c = (x + 1) / 10;
-            in
-              builtins.toString (x + 1 - (c * 10));
-          in ''
-            bind = $mod, ${ws}, workspace, ${toString (x + 1)}
-            bind = $mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}
-          ''
-        )
-      10) }
-
-      # ...
   '';
   wayland.windowManager.hyprland.enable = true;
 }
