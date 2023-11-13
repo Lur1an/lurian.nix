a1fca7f2b5f68defc5ac976bdfbd220afee508b8
lurian <lurian-code@protonmail.com>
2023-11-13 09:53:57 +0100

    feat(services.xserver): Enable i3 window manager and lightdm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -97,6 +97,18 @@
 
   services.xserver = {
     enable = true;
+    windowManager.i3 = {
+      enable = true;
+    };
+    displayManager = {
+      lightdm = {
+        enable = true;
+        greeters.gtk = {
+          enable = true;
+        };
+      };
+      defaultSession = "none+i3";
+    };
     videoDrivers = [ "nvidia" ];
   };
 
