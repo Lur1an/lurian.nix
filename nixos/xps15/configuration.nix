0d7e4325dc1ced2598d6e4bb27fbc999543e10d4
lurian <lurian-code@protonmail.com>
2023-11-13 19:51:14 +0100

    refactor(nixos config): Update PCI Bus ID values

--- nixos/xps15/configuration.nix
+++ nixos/xps15/configuration.nix
@@ -21,8 +21,8 @@
   hardware.nvidia.prime = {
       sync.enable = true;
       # Make sure to use the correct Bus ID values for your system!
-      # nvidiaBusId = "PCI:14:0:0";
-      # intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
   };
 
   services.xserver.libinput = {
