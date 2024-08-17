08233e76f99e8f9748cbe5dcd99e68e5a34e4358
lur1an <lurian-code@protonmail.com>
2024-08-17 20:59:04 +0200

    remove winwrap hypr

--- home-manager/dev/node.nix
+++ home-manager/dev/node.nix
@@ -6,11 +6,11 @@
     typescript
     nodePackages.npm # Package manager
     nodePackages.pnpm # Package manager
+    nodePackages.js-yaml
     nodePackages.node2nix
     nodePackages.live-server # Live server
     nodePackages.typescript-language-server # Typescript
     nodePackages_latest.eslint_d # JS linter
     nodePackages_latest.prettier # Formatter
-    nodePackages_latest.js-yaml
   ];
 }
--- home-manager/hyprland/hypr/default.nix
+++ home-manager/hyprland/hypr/default.nix
@@ -34,9 +34,9 @@ in {
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprwinwrap
-    ];
+    # plugins = [
+    #   inputs.hyprland-plugins.packages.${pkgs.system}.hyprwinwrap
+    # ];
     xwayland.enable = true;
     settings = {
       env = [
