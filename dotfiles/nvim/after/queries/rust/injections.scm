05ea289c2ea49981e9dbd736010e536a25ae677e
lur1an <lurian-code@protonmail.com>
2024-12-12 10:16:08 +0100

    update injections

--- dotfiles/nvim/after/queries/rust/injections.scm
+++ dotfiles/nvim/after/queries/rust/injections.scm
@@ -6,3 +6,47 @@
   (#match? @variable.name ".*js.*")
   (#set! injection.language "javascript")
 )
+
+; sqlx::query! raw string
+((macro_invocation
+  (scoped_identifier
+    path: (identifier) @_path
+    name: (identifier) @_name)
+  (token_tree
+    . (raw_string_literal (string_content) @injection.content))
+ (#eq? @_name "query")
+ (#eq? @_path "sqlx"))
+ (#set! injection.language "sql"))
+
+; sqlx::query! normal string
+((macro_invocation
+  (scoped_identifier
+    path: (identifier) @_path
+    name: (identifier) @_name)
+  (token_tree
+    . (string_literal (string_content) @injection.content))
+ (#eq? @_name "query")
+ (#eq? @_path "sqlx"))
+ (#set! injection.language "sql"))
+
+; sqlx::query_as! raw string
+((macro_invocation
+  (scoped_identifier
+    path: (identifier) @_path
+    name: (identifier) @_name)
+  (token_tree
+    . (identifier) (string_literal (string_content) @injection.content))
+ (#eq? @_name "query_as")
+ (#eq? @_path "sqlx"))
+ (#set! injection.language "sql"))
+
+; sqlx::query_as! normal string
+((macro_invocation
+  (scoped_identifier
+    path: (identifier) @_path
+    name: (identifier) @_name)
+  (token_tree
+    . (identifier) (raw_string_literal (string_content) @injection.content))
+ (#eq? @_name "query_as")
+ (#eq? @_path "sqlx"))
+ (#set! injection.language "sql"))
