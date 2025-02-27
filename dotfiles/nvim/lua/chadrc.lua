e3b95a1bdafd96b9c5e2ec208b03d5041e75cd7d
lur1an <lurian-code@protonmail.com>
2025-02-27 17:09:14 +0100

    update

--- dotfiles/nvim/lua/chadrc.lua
+++ dotfiles/nvim/lua/chadrc.lua
@@ -5,7 +5,7 @@ local transparent = true
 local in_tmux = os.getenv("TMUX") ~= nil
 
 M.base46 = {
-	theme = "bearded-arc",
+	theme = "catppuccin",
 	transparency = transparent,
 	integrations = { "dap", "trouble" },
 }
@@ -26,7 +26,7 @@ M.term = {
 }
 
 M.ui = {
-	theme_toggle = { "bearded-arc", "bearded-arc" },
+	theme_toggle = { "catppuccin", "catppuccin" },
 	telescope = {
 		style = "bordered", -- borderless / bordered
 	},
