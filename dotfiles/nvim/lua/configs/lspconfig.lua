517042d920bb7942dc2e3bab10ae0ced82221385
lur1an <lurian-code@protonmail.com>
2025-09-18 10:49:47 +0200

    update lspconfig to new API

--- dotfiles/nvim/lua/configs/lspconfig.lua
+++ dotfiles/nvim/lua/configs/lspconfig.lua
@@ -4,49 +4,52 @@ local on_attach = configs.on_attach
 local on_init = configs.on_init
 local capabilities = configs.capabilities
 
-local lspconfig = require("lspconfig")
-
+-- List of language servers to configure
 local servers = {
 	"docker_compose_language_service",
 	"dockerls",
 	"tailwindcss",
 	"svelte",
-  "terraformls",
+	"terraformls",
 	"ansiblels",
 	"graphql",
 	"ts_ls",
-  "just",
+	"just",
 	"ruff",
 	"nixd",
 	"helm_ls",
 	"marksman",
 }
 
+-- Configure all servers with default settings
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
+	vim.lsp.config(lsp, {
 		on_init = on_init,
 		on_attach = on_attach,
 		capabilities = capabilities,
 	})
+	vim.lsp.enable(lsp)
 end
--- read PYTHON_LSP env variable to choose between pyright and basedpyright
+
+-- Python LSP configuration with environment variable support
 local python_lsp = os.getenv("PYTHON_LSP") or "basedpyright"
 
 if python_lsp == "pyright" then
-	lspconfig.pyright.setup({
+	vim.lsp.config("pyright", {
 		on_init = on_init,
 		on_attach = on_attach,
 		capabilities = capabilities,
 	})
+	vim.lsp.enable("pyright")
 else
-	lspconfig.basedpyright.setup({
+	vim.lsp.config("basedpyright", {
 		on_init = on_init,
 		on_attach = on_attach,
 		capabilities = capabilities,
 		settings = {
 			basedpyright = {
 				analysis = {
-          diagnosticMode = "workspace",
+					diagnosticMode = "workspace",
 					extraPaths = {
 						"./.devenv/state/venv/lib/python3.12/site-packages",
 					},
@@ -55,4 +58,5 @@ else
 			},
 		},
 	})
+	vim.lsp.enable("basedpyright")
 end
