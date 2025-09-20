58e9403ff4f44b4622a3cbac08000e2d9ecd063e
lur1an <lurian-code@protonmail.com>
2025-09-20 09:12:21 +0200

    overlay opencode for latest version

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,30 @@
   # This one contains whatever you want to overlay
   modifications = final: prev: {
     ollama = prev.ollama.override {acceleration = "cuda";};
+    
+    opencode = prev.opencode.overrideAttrs (oldAttrs: rec {
+      version = "0.10.3";
+      src = oldAttrs.src.override {
+        tag = "v${version}";
+        hash = "sha256-NE24parzEawK66AFhqQbKCM3Y4uSxAjHIOpnnAcL6ec=";
+      };
+      tui = oldAttrs.tui.overrideAttrs (old: {
+        vendorHash = "sha256-H+TybeyyHTbhvTye0PCDcsWkcN8M34EJ2ddxyXEJkZI=";
+      });
+      node_modules = oldAttrs.node_modules.overrideAttrs (old: {
+        buildPhase = ''
+          runHook preBuild
+          export BUN_INSTALL_CACHE_DIR=$(mktemp -d)
+          bun install \
+            --filter=opencode \
+            --force \
+            --ignore-scripts \
+            --no-progress
+          runHook postBuild
+        '';
+        outputHash = "sha256-fGf2VldMlxbr9pb3B6zVL+fW1S8bRjefJW+jliTO73A=";
+      });
+    });
   };
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
