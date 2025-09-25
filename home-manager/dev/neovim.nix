bc44ac921baf5efc4833338213d2f63da3295747
lur1an <lurian-code@protonmail.com>
2025-09-25 21:01:39 +0200

    update

--- home-manager/dev/neovim.nix
+++ home-manager/dev/neovim.nix
@@ -31,7 +31,6 @@ in {
     [
       gnumake
       fd
-      isort
       ruff
       tree-sitter
       (vscode-with-extensions.override {
@@ -49,10 +48,4 @@ in {
   home.file.".ideavimrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/lurian.nix/dotfiles/.ideavimrc";
 
   home.file.".vscode-lldb".source = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb";
-
-  home.sessionVariables = {
-    PYLANCE_PATH = "${pkgs.vscode-extensions.ms-python.vscode-pylance}";
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-    JAVA_21_HOME = "${pkgs.jdk21}";
-  };
 }
