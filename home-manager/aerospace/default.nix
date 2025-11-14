4b7fe386331ecdfa0c364e3cad8c412f354c31a9
lur1an <lurian-code@protonmail.com>
2025-11-15 00:30:54 +0100

    fix aerospace

--- home-manager/aerospace/default.nix
+++ home-manager/aerospace/default.nix
@@ -1,6 +1,9 @@
-{config, ...}: {
-  programs.aerospace = {
-    enable = true;
-  };
+{config, pkgs, ...}: {
+  # programs.aerospace = {
+  #   enable = true;
+  # };
+  home.packages = with pkgs; [
+    aerospace
+  ];
   xdg.configFile."aerospace/aerospace.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/lurian.nix/home-manager/aerospace/aerospace.toml";
 }
