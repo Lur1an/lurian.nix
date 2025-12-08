778a9d0473308ed7e748b8b8e987f660da419d87
lur1an <lurian-code@protonmail.com>
2025-12-08 20:41:57 +0100

    add playwright mcp

--- home-manager/terminal/opencode.nix
+++ home-manager/terminal/opencode.nix
@@ -8,9 +8,14 @@
   cfg = config.terminal;
 in {
   config = lib.mkIf cfg.opencode.enable {
-    home.packages = [
+    home.packages = with pkgs; [
       inputs.opencode.packages.${pkgs.system}.default
+      playwright-mcp
+      playwright
     ];
+    home.sessionVariables = {
+      PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright.browsers}";
+    };
     xdg.configFile."opencode".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/lurian.nix/dotfiles/opencode";
   };
 }
