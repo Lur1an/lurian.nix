27ab18e9ebc99f405a517580bd7fd3ba38cfdafb
lur1an <lurian-code@protonmail.com>
2026-04-12 18:32:47 +0200

    battery module

--- home-manager/profiles/zephyrus.nix
+++ home-manager/profiles/zephyrus.nix
@@ -1,5 +1,60 @@
 {...}: {
-  programs.waybar.settings.mainBar.output = ["eDP-1"];
+  programs.waybar = {
+    settings.mainBar = {
+      output = ["eDP-1"];
+
+      modules-right = [
+        "battery"
+      ];
+
+      battery = {
+        states = {
+          warning = 30;
+          critical = 15;
+        };
+        format = "{icon} {capacity}%";
+        format-charging = "󰂄 {capacity}%";
+        format-plugged = "󰚥 {capacity}%";
+        format-full = "󰁹 Full";
+        format-icons = [
+          "󰂎"
+          "󰁺"
+          "󰁻"
+          "󰁼"
+          "󰁽"
+          "󰁾"
+          "󰁿"
+          "󰂀"
+          "󰂁"
+          "󰂂"
+          "󰁹"
+        ];
+        tooltip-format = "{timeTo}\n{power:.1f}W\n{health}%";
+      };
+    };
+
+    style = ''
+      #battery {
+        padding: 0 10px;
+        color: @on-surface;
+      }
+
+      #battery.warning {
+        color: @error;
+      }
+
+      #battery.critical {
+        color: @error;
+        animation: blink 1s infinite;
+      }
+
+      @keyframes blink {
+        to {
+          color: @on-surface;
+        }
+      }
+    '';
+  };
 
   imports = [
     ./linux.nix
