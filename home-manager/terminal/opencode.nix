09f0abff03607348e863279fff84429500edfac5
lur1an <lurian-code@protonmail.com>
2026-08-26 17:46:29 +0200

    opencode superpowers

--- home-manager/terminal/opencode.nix
+++ home-manager/terminal/opencode.nix
@@ -5,6 +5,12 @@
   ...
 }: let
   cfg = config.terminal;
+  superpowers = pkgs.fetchFromGitHub {
+    owner = "obra";
+    repo = "superpowers";
+    rev = "v6.3.0";
+    hash = "sha256-EsGNO0dULWf5Bx6bGrCv2kI2Z8aKH0kRvGiuN23wChQ=";
+  };
 in {
   config = lib.mkIf cfg.opencode.enable {
     programs.opencode = {
@@ -14,6 +20,7 @@ in {
 
       settings = {
         permission.external_directory."~/.cargo/registry/**" = "allow";
+        plugin = ["file://${superpowers}/.opencode/plugins/superpowers.js"];
       };
 
       tui = {
