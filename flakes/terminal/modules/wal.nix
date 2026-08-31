a3ba22fccf9b5fc89a0f754ed37d8956e66268d0
lur1an <lurian-code@protonmail.com>
2026-08-31 15:00:24 +0200

    fix: wal home path conflict on upgrade to modularized flake structure

--- flakes/terminal/modules/wal.nix
+++ flakes/terminal/modules/wal.nix
@@ -5,6 +5,7 @@
   ...
 }: let
   cfg = config.lurian.terminal.wal;
+  legacyTemplates = "${config.xdg.configHome}/wal/templates";
 in {
   config = lib.mkMerge [
     {
@@ -13,6 +14,15 @@ in {
     }
     (lib.mkIf cfg.enable {
       home.packages = [pkgs.pywal16 pkgs.imagemagick];
+      # Older revisions managed the whole templates directory as one store
+      # symlink. Remove that parent link before Home Manager creates per-file
+      # links; otherwise it tries to modify the read-only store directory.
+      home.activation.removeLegacyWalTemplates =
+        lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+          if [[ -L ${lib.escapeShellArg legacyTemplates} ]]; then
+            $DRY_RUN_CMD ${pkgs.coreutils}/bin/rm -f -- ${lib.escapeShellArg legacyTemplates}
+          fi
+        '';
       xdg.configFile = lib.mapAttrs' (name: source:
         lib.nameValuePair "wal/templates/${name}" {inherit source;})
       cfg.templates;
